4a85347b1806f7cb00cc6c63c75126af58083948
PortableDoughnut <gwen.thelin@gmail.com>
2024-09-19 10:07:33 -0600

    Initial Commit

---
+++ Hobby Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by Gwen Thelin on 9/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hobby TutorialTests/Hobby_TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hobby_TutorialTests.swift
+//  Hobby TutorialTests
+//
+//  Created by Gwen Thelin on 9/19/24.
+//
+
+import Testing
+@testable import Hobby_Tutorial
+
+struct Hobby_TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
